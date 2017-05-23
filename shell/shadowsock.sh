2fdfe76ea5bff17f74e57bb892b6ef7ef2984f11
lessYang <haiwei042@gmail.com>
2017-05-23 14:21:47 +0800

    增加shadowsock安装

---
+++ shell/shadowsock.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo add-apt-repository ppa:hzwhuang/ss-qt5
+sudo apt-get update
+sudo apt-get install shadowsocks-qt5
+
+
+
+###########
+#  chrome 需要安装ProxySwitchyOmega, package中可以找到
+#
